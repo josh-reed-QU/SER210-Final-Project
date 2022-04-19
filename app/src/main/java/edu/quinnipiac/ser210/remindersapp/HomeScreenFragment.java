2be047d6fe73b61e6fbe897193cb3514755efd31
josh-reed-QU <joshreed114@gmail.com>
2022-04-19 13:48:08 -0400

    Second Commit (Skeleton UI)

---
+++ app/src/main/java/edu/quinnipiac/ser210/remindersapp/HomeScreenFragment.java
@@ -0,0 +1,40 @@
+package edu.quinnipiac.ser210.remindersapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeScreenFragment extends Fragment implements View.OnClickListener{
+    NavController navController = null;
+
+    public HomeScreenFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_screen, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        //TODO: get buttons from screen, implement onClickListener here
+    }
+
+    @Override
+    public void onClick(View view) {
+        //TODO: implement for the category buttons
+    }
+}
---
+++ app/src/main/java/edu/quinnipiac/ser210/remindersapp/SplashScreenFragment.java
@@ -0,0 +1,40 @@
+package edu.quinnipiac.ser210.remindersapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+public class SplashScreenFragment extends Fragment implements View.OnClickListener {
+    // NavController variable to allow for fragment navigation
+    NavController navController = null;
+
+    public SplashScreenFragment() {
+        // Required default empty constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        //TODO: add a button to splash screen, implement onClickListener Here, will always have the all category
+    }
+
+    @Override
+    public void onClick(View view) {
+        //TODO: navigate to next screen using navController
+    }
+}
